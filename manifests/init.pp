113b3056f86bbb6f7e20dc98d61632513e8dc2eb
Jordi Prats <jprats@systemadmin.es>
2016-02-04 13:50:20 +0100

    lint tabs

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,101 +35,101 @@
 #
 # Copyright 2015 Your name here, unless otherwise noted.
 #
-class nginx 	(
-			$workerprocesses=$nginx::params::workerprocesses_default,
-			$servertokens=$nginx::params::servertokens_default,
-			$gziptypes=$nginx::params::gziptypes_default,
-			#default vhost
-			$defaultdocroot='/var/www/default',
-			$serverstatus_url='/server-status',
-			$serverstatus_allowedips=[ '127.0.0.1' ],
-			$username=$nginx::params::username,
-			$pidfile='/var/run/nginx.pid',
-			$add_default_vhost=true,
-		) inherits params{
+class nginx   (
+      $workerprocesses=$nginx::params::workerprocesses_default,
+      $servertokens=$nginx::params::servertokens_default,
+      $gziptypes=$nginx::params::gziptypes_default,
+      #default vhost
+      $defaultdocroot='/var/www/default',
+      $serverstatus_url='/server-status',
+      $serverstatus_allowedips=[ '127.0.0.1' ],
+      $username=$nginx::params::username,
+      $pidfile='/var/run/nginx.pid',
+      $add_default_vhost=true,
+    ) inherits params{
 
-	validate_absolute_path($defaultdocroot)
+  validate_absolute_path($defaultdocroot)
 
-	validate_array($serverstatus_allowedips)
+  validate_array($serverstatus_allowedips)
 
-	Exec {
-		path => '/usr/sbin:/usr/bin:/sbin:/bin',
-	}
+  Exec {
+    path => '/usr/sbin:/usr/bin:/sbin:/bin',
+  }
 
-	if($nginx::params::include_epel)
-	{
-		include epel
-	}
+  if($nginx::params::include_epel)
+  {
+    include epel
+  }
 
-	package { $nginx::params::package:
-		ensure  => 'installed',
-		require => $nginx::params::require_epel,
-	}
+  package { $nginx::params::package:
+    ensure  => 'installed',
+    require => $nginx::params::require_epel,
+  }
 
-	file { '/etc/nginx/nginx.conf':
-		ensure  => present,
-		owner   => "root",
-		group   => "root",
-		mode    => 0644,
-		require => Package[$nginx::params::package],
-		notify  => Service['nginx'],
-		content => template("${module_name}/nginx.erb")
-	}
+  file { '/etc/nginx/nginx.conf':
+    ensure  => present,
+    owner   => "root",
+    group   => "root",
+    mode    => 0644,
+    require => Package[$nginx::params::package],
+    notify  => Service['nginx'],
+    content => template("${module_name}/nginx.erb")
+  }
 
-	exec { "mkdir_p_${defaultdocroot}":
-		command => "mkdir -p ${defaultdocroot}",
-		require => File['/etc/nginx/nginx.conf'],
-		creates => $defaultdocroot,
-	}
+  exec { "mkdir_p_${defaultdocroot}":
+    command => "mkdir -p ${defaultdocroot}",
+    require => File['/etc/nginx/nginx.conf'],
+    creates => $defaultdocroot,
+  }
 
-	exec { "mkdir_p_${sites_dir}":
-		command => "mkdir -p ${sites_dir}",
-		require => File['/etc/nginx/nginx.conf'],
-		creates => $sites_dir,
-	}
+  exec { "mkdir_p_${sites_dir}":
+    command => "mkdir -p ${sites_dir}",
+    require => File['/etc/nginx/nginx.conf'],
+    creates => $sites_dir,
+  }
 
-	exec { "mkdir_p_${sites_enabled_dir}":
-		command => "mkdir -p ${sites_enabled_dir}",
-		require => File['/etc/nginx/nginx.conf'],
-		creates => $sites_enabled_dir,
-	}
+  exec { "mkdir_p_${sites_enabled_dir}":
+    command => "mkdir -p ${sites_enabled_dir}",
+    require => File['/etc/nginx/nginx.conf'],
+    creates => $sites_enabled_dir,
+  }
 
-	if($add_default_vhost)
-	{
-		file { "${nginx::params::sites_dir}/default":
-			ensure  => 'present',
-			owner   => 'root',
-			group   => 'root',
-			mode    => '0644',
-			content => template("${module_name}/default_vhost_template.erb"),
-			notify  => Service['nginx'],
-			before  => Service['nginx'],
-			require => Exec [
-												"mkdir_p_${defaultdocroot}",
-												"mkdir_p_${sites_dir}",
-												"mkdir_p_${sites_enabled_dir}"
-											],
-		}
+  if($add_default_vhost)
+  {
+    file { "${nginx::params::sites_dir}/default":
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template("${module_name}/default_vhost_template.erb"),
+      notify  => Service['nginx'],
+      before  => Service['nginx'],
+      require => Exec [
+                        "mkdir_p_${defaultdocroot}",
+                        "mkdir_p_${sites_dir}",
+                        "mkdir_p_${sites_enabled_dir}"
+                      ],
+    }
 
-		file { "${nginx::params::sites_enabled_dir}/default":
-			ensure  => "${nginx::params::sites_dir}/default",
-			require => File["${nginx::params::sites_dir}/default"],
-			notify  => Service['nginx'],
-		}
-	}
+    file { "${nginx::params::sites_enabled_dir}/default":
+      ensure  => "${nginx::params::sites_dir}/default",
+      require => File["${nginx::params::sites_dir}/default"],
+      notify  => Service['nginx'],
+    }
+  }
 
-	if($nginx::params::purge_default_vhost!=undef)
-	{
-		file { $nginx::params::purge_default_vhost:
-			ensure  => 'absent',
-			require => Package[$nginx::params::package],
-			notify  => Service['nginx'],
-		}
-	}
+  if($nginx::params::purge_default_vhost!=undef)
+  {
+    file { $nginx::params::purge_default_vhost:
+      ensure  => 'absent',
+      require => Package[$nginx::params::package],
+      notify  => Service['nginx'],
+    }
+  }
 
-	service { 'nginx':
-		ensure  => 'running',
-		enable  => true,
-	}
+  service { 'nginx':
+    ensure  => 'running',
+    enable  => true,
+  }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,52 +1,52 @@
 class nginx::params {
 
 
-	$sites_dir='/etc/nginx/sites-available'
-	$sites_enabled_dir='/etc/nginx/sites-enabled'
-	$servertokens_default='off'
-	$gziptypes_default= [ 'text/plain', 'text/css', 'application/json', 'application/x-javascript', 'text/xml', 'application/xml', 'application/xml+rss', 'text/javascript' ]
+  $sites_dir='/etc/nginx/sites-available'
+  $sites_enabled_dir='/etc/nginx/sites-enabled'
+  $servertokens_default='off'
+  $gziptypes_default= [ 'text/plain', 'text/css', 'application/json', 'application/x-javascript', 'text/xml', 'application/xml', 'application/xml+rss', 'text/javascript' ]
 
-	case $::osfamily
-	{
-		'redhat':
-		{
-			case $::operatingsystemrelease
-			{
-				/^[67].*$/:
-				{
-					$purge_default_vhost='/etc/nginx/conf.d/default.conf'
-					$include_epel=true
-					$require_epel=Class[ 'epel' ]
-					$package='nginx'
-					$username='nginx'
-					$workerprocesses_default=$::processorcount
-				}
-				default: { fail("Unsupported RHEL/CentOS version!")  }
-			}
-		}
-		'Debian':
-		{
-			case $::operatingsystem
-			{
-				'Ubuntu':
-				{
-					case $::operatingsystemrelease
-					{
-						/^14.*$/:
-						{
-							$package="nginx-light"
-						  $include_epel=false
-							$username='www-data'
-							$workerprocesses_default='auto'
-						}
-						default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
-					}
-				}
-				'Debian': { fail("Unsupported")  }
-				default: { fail("Unsupported Debian flavour!")  }
-			}
-		}
-		default: { fail("Unsupported OS!")  }
-	}
+  case $::osfamily
+  {
+    'redhat':
+    {
+      case $::operatingsystemrelease
+      {
+        /^[67].*$/:
+        {
+          $purge_default_vhost='/etc/nginx/conf.d/default.conf'
+          $include_epel=true
+          $require_epel=Class[ 'epel' ]
+          $package='nginx'
+          $username='nginx'
+          $workerprocesses_default=$::processorcount
+        }
+        default: { fail("Unsupported RHEL/CentOS version!")  }
+      }
+    }
+    'Debian':
+    {
+      case $::operatingsystem
+      {
+        'Ubuntu':
+        {
+          case $::operatingsystemrelease
+          {
+            /^14.*$/:
+            {
+              $package="nginx-light"
+              $include_epel=false
+              $username='www-data'
+              $workerprocesses_default='auto'
+            }
+            default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
+          }
+        }
+        'Debian': { fail("Unsupported")  }
+        default: { fail("Unsupported Debian flavour!")  }
+      }
+    }
+    default: { fail("Unsupported OS!")  }
+  }
 
 }
