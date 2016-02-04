1d744f8e0a0b179b62de6e760e9b23a9385a739a
Jordi Prats <jprats@systemadmin.es>
2016-02-04 12:18:05 +0100

    allowed i denied ips per stub status

--- manifests/stubstatus.pp
+++ manifests/stubstatus.pp
@@ -1,7 +1,24 @@
 define nginx::stubstatus (
                           $stubstatus_url='/server-status',
                           $servername=$name,
+                          $allowed_ips=undef,
+                          $denied_ips=undef,
                         ) {
+
+  if($allowed_ips!=undef and $denied_ips!=undef)
+  {
+    fail('Incompatible variables: allowed_ips and denied_ips')
+  }
+
+  if($allowed_ips!=undef)
+  {
+    validate_array($allowed_ips)
+  }
+
+  if($denied_ips!=undef)
+  {
+    validate_array($denied_ips)
+  }
 
   concat::fragment{ "${nginx::params::sites_dir}/${servername} ${stubstatus_url} stubstatus":
     target  => "${nginx::params::sites_dir}/${servername}",
