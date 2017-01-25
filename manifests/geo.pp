544eaed76b4801ca3804bc5fdec04d7c82313a3a
Jordi Prats <jprats@systemadmin.es>
2017-01-25 15:02:36 +0100

    afegit geo

---
+++ manifests/geo.pp
@@ -0,0 +1,20 @@
+# geo $rucs {
+#   default 0;
+#   194.116.240.25/32 1;
+# }
+define nginx::geo (
+                    $geo_name  = $name,
+                    $default   = undef,
+                    $variables = undef,
+                  ) {
+
+  validate_hash($variables)
+
+  file { "${nginx::params::conf_d_dir}/geo_${geo_name}.conf":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/geo.erb"),
+  }
+}
