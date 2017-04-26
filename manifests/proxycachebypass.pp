68ac4ab04bf5bff70eafa0bf5f6b008de58bc523
Jordi Prats <jprats@systemadmin.es>
2017-04-26 18:23:44 +0200

    lint

--- manifests/proxycachebypass.pp
+++ manifests/proxycachebypass.pp
@@ -7,7 +7,7 @@ define nginx::proxycachebypass(
   #fragment name
   $proxypass_url_clean = regsubst($proxypass_url, '[^a-zA-Z]+', '_')
 
-  concat::fragment{ "${nginx::params::sites_dir}/${port}_${servername} proxypass header ${header}":
+  concat::fragment{ "${nginx::params::sites_dir}/${port}_${servername} proxypass header ${bypass}":
     target  => "${nginx::params::sites_dir}/${port}_${servername}",
     order   => "10 - ${proxypass_url_clean}-01",
     content => template("${module_name}/vhost/proxy/proxycachebypass.erb"),
