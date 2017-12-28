b9f7b42bfdee699f77b9ea3d9e1759f8208d6ced
Jordi Prats <jprats@systemadmin.es>
2017-12-28 18:57:06 +0100

    clenaup

--- manifests/proxyredirect.pp
+++ manifests/proxyredirect.pp
@@ -5,12 +5,10 @@ define nginx::proxyredirect (
                               $servername    = $name,
                               $port          = '80',
                             ) {
-  #fragment name
-  $proxypass_url_clean = regsubst($proxypass_url, '[^a-zA-Z]+', '_')
 
   concat::fragment{ "${nginx::params::sites_dir}/${port}_${servername} proxypass header ${bypass}":
     target  => "${nginx::params::sites_dir}/${port}_${servername}",
-    order   => "10 - ${proxypass_url_clean}-02",
+    order   => "10 - redirect ${redirect_from} ${redirect_to} -02",
     content => template("${module_name}/vhost/proxy/proxyredirect.erb"),
   }
 }
