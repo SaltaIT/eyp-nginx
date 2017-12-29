27476654d889961292c22fa4458e71d8f42dc78b
Jordi Prats <jprats@systemadmin.es>
2017-12-29 15:48:19 +0100

    bugfix

--- manifests/redirect.pp
+++ manifests/redirect.pp
@@ -10,16 +10,6 @@ define nginx::redirect (
                         ) {
   include ::nginx
 
-  if($path==undef and $match==undef)
-  {
-    fail('path and match are undef, WTF man...')
-  }
-
-  if($path!=undef and $match!=undef)
-  {
-    fail('path and match cannot be defined at the same time, WTF man...')
-  }
-
   concat::fragment{ "${apache::params::baseconf}/conf.d/sites/${order}-${servername}-${port}.conf.run redirect ${match} ${path} ${url}":
     target  => "${apache::params::baseconf}/conf.d/sites/${order}-${servername}-${port}.conf.run",
     content => template("${module_name}/vhost/redirects.erb"),
