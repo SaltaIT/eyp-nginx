1e1135258bd7fd5c62358504a1810d530c07bb36
Jordi Prats <jprats@systemadmin.es>
2017-12-29 15:27:12 +0100

    neteja

--- manifests/proxyredirect.pp
+++ manifests/proxyredirect.pp
@@ -10,7 +10,7 @@ define nginx::proxyredirect (
 
   concat::fragment{ "${nginx::params::sites_dir}/${port}_${servername} proxyredirect ${redirect} ${redirect_from_clean} ${redirect_to_clean}":
     target  => "${nginx::params::sites_dir}/${port}_${servername}",
-    order   => "10_redirect__${redirect_from_clean}__${redirect_to_clean}__02",
+    order   => "10_redirect_02",
     content => template("${module_name}/vhost/proxy/proxyredirect.erb"),
   }
 }
