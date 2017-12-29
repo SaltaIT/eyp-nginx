0ef62d34a019eda5912874ab695f3bff36e2c564
Jordi Prats <jprats@systemadmin.es>
2017-12-29 10:16:20 +0100

    nom proxyredirect

--- manifests/proxyredirect.pp
+++ manifests/proxyredirect.pp
@@ -6,9 +6,9 @@ define nginx::proxyredirect (
                               $port          = '80',
                             ) {
 
-  concat::fragment{ "${nginx::params::sites_dir}/${port}_${servername} proxypass header ${bypass}":
+  concat::fragment{ "${nginx::params::sites_dir}/${port}_${servername} proxyredirect ${redirect} ${redirect_from} ${redirect_to}":
     target  => "${nginx::params::sites_dir}/${port}_${servername}",
-    order   => "10 - redirect ${redirect_from} ${redirect_to} -02",
+    order   => "10 - redirect ${redirect_from} ${redirect} ${redirect_from} ${redirect_to} - 02",
     content => template("${module_name}/vhost/proxy/proxyredirect.erb"),
   }
 }
